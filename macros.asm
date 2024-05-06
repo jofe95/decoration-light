7dc18032c17bc52ce9c63724a40e4c99a7a85938
Johannes Fechner <130906244+jofe95@users.noreply.github.com>
2024-05-06 21:53:43 +0200

    Update macros.asm

--- macros.asm
+++ macros.asm
@@ -1,36 +1,41 @@
-; File: macros.asm
+; File:     macros.asm
+; Device:   AVR
+; Version:  2024-04-25
+; Author:   Johannes Fechner
+;           https://4n7.de/
+;           https://github.com/jofe95
 
 #define ROUND(X) (int(1.0*(X)+0.5))
 #define FLOOR(X) (int(X))
 #define CEIL(X) (frac(X) > 0 ? int(X)+1 : int(X))
 
 .macro xout ; eXtended "out"
-	.if @0 > 0x3F
-		sts @0, @1
-	.else
-		out @0, @1
-	.endif
+    .if @0 > 0x3F
+        sts @0, @1
+    .else
+        out @0, @1
+    .endif
 .endmacro
 
 .macro xin ; eXtended "in"
-	.if @1 > 0x3F
-		lds @0, @1
-	.else
-		in  @0, @1
-	.endif
+    .if @1 > 0x3F
+        lds @0, @1
+    .else
+        in  @0, @1
+    .endif
 .endmacro
 
 .macro ldiz ; Load immediate into Z double register.
-	ldi		ZH, high(@0)
-	ldi		ZL, low(@0)
+    ldi     ZH, high(@0)
+    ldi     ZL, low(@0)
 .endmacro
 
 .macro addz ; Add register to Z double register; register 'reg00' must be cleared.
-	add 	ZL, @0
-	adc		ZH, reg00
+    add     ZL, @0
+    adc     ZH, reg00
 .endmacro
 
 .macro addi16 ; RdH, RdL, k
-	subi    @1, low(-@2)
-	sbci    @0, high(-@2)
+    subi    @1, low(-@2)
+    sbci    @0, high(-@2)
 .endmacro
