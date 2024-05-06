a4818752fa12461beb14da5c397051cc6a401d0a
Johannes Fechner <130906244+jofe95@users.noreply.github.com>
2024-05-06 21:55:40 +0200

    Update and rename remote-control-panasonic-n2qayb-table.asm to irrc-panasonic-n2qayb.asm

--- irrc-panasonic-n2qayb.asm
+++ irrc-panasonic-n2qayb.asm
@@ -1,12 +1,13 @@
-; File:			remote-control-panasonic-n2qayb.defs.asm
-; Device:		AVR
-; Version:		2024-04-05
-; Description:	This file defines a table in ROM which associates each byte
-;				value (0..255) with an ID which specifies the button meaning,
-;				or RC_COMMANDS (the count of IDs) in case the byte is not
-;				sent by any button of the remote control.
-; Author:		Johannes Fechner
-;				https://www.mikrocontroller.net/user/show/jofe
+; File:         irrc-panasonic-n2qayb.asm
+; Device:       AVR
+; Version:      2024-04-05
+; Description:  This file defines a table in ROM which associates each byte
+;               value (0..255) with an ID which specifies the button meaning,
+;               or RC_COMMANDS (the count of IDs) in case the byte is not
+;               sent by any button of the remote control.
+; Author:       Johannes Fechner
+;               https://4n7.de/
+;               https://github.com/jofe95
 
 ; The first address-like bytes sent by the Kaseikyo remote control:
 .equ RC_CONST_0 = $02
@@ -35,13 +36,5 @@ rcCmdIdTable:
 .db 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20
 
 ; Buttons "0".."9" -> 0..9.
-;.equ RC_CMD_POWER = 10
 .equ RC_CMD_MENU = 11
 .equ RC_CMD_INFO = 12
-;.equ RC_CMD_PLUS = 13
-;.equ RC_CMD_BACK = 14
-;.equ RC_CMD_SKIP_LEFT = 15
-;.equ RC_CMD_PLAY = 16
-;.equ RC_CMD_SKIP_RIGHT = 17
-;.equ RC_CMD_MINUS = 18
-;.equ RC_CMD_CANCEL = 19
